56bf58eae0aba5c5e4dd9e245c6625f54672ffa7
Olivier Zurkirchen <oz@x02.ch>
2026-03-19 10:52:08 +0100

    bundled with jQuery

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ function cssTask() {
 
 function jsTask() {
     return src([
+            'node_modules/jquery/dist/jquery.min.js',
             'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
             'node_modules/@studio-freight/lenis/dist/lenis.min.js',
             'src/roessli.js'
